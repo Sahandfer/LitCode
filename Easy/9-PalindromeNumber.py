742fcd97acab148938322b47912b52efc7fdd34b
unknown <karimiali0022@yahoo.com>
2020-04-20 16:57:25 +0800

    Problem 9 - Palindrome Number pwned

---
+++ Easy/9-PalindromeNumber.py
@@ -0,0 +1,19 @@
+"""
+:type x: int
+:rtype: bool
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        strX = str(x)
+        endIndex = len(strX) - (0 if (len(strX) % 2 == 0) else 1)
+        for i in range(endIndex):
+            if strX[i] != strX[len(strX) - i - 1]:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    # Test Cases
+    print(Solution().isPalindrome(121))
